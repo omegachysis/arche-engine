5885816a0a65e64daf0c17fafc4512b29e264b37
Matthew Robinson <omegachysis@outlook.com>
2014-04-25 21:54:51 -0400

    Added a second demo

--- demo 001-MinimumFramework.py
+++ demo 001-MinimumFramework.py
@@ -5,13 +5,13 @@ import arche
 # Create a logging object for this file
 log = arche.debug.log("main")
 
-# Log the successful loading of this file
-log.info("starting demo 001")
-
 def main():
+    # Log the successful loading of this file
+    log.info("starting demo 001")
+    
     # Start a default game engine
     game = arche.engine.Game(width = 1280, height = 800, fullscreen = False,
-                             titleName = "ArcheEngine Demo - Simple Application")
+                             titleName = "ArcheEngine Demo - Minimum Framework")
     game.run()
 
 if __name__ == "__main__":
---
+++ demo 002 - SimpleApplication.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import arche
+
+log = arche.debug.log("main")
+
+def main():
+    log.info("starting demo 002")
+
+    game = arche.engine.Game(width = 1280, height = 800, fullscreen = False,
+                             titleName = "ArcheEngine Demo - Simple Application")
+
+    # Start our simple application.  It will not run until game.run() is called though.
+    SimpleApplication().start()
+    
+    game.run()
+
+# Applications are used to organize various parts of your game.
+# They are analogous to PowerPoint slides or separate slides
+#  in a slide show.
+class SimpleApplication(arche.engine.Application):
+    def __init__(self):
+        super().__init__() # run this at the beginning of every class derivation
+
+        # Change the background color to dark grey
+        self.backgroundColor = (80,80,80)
+
+if __name__ == "__main__":
+    arche.debug.test(main)
