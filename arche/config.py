cdbab179936f63c0c478a5ce4f6a545d14b32454
Matthew Robinson <omegachysis@outlook.com>
2014-04-26 08:55:06 -0400

    Created timestamps for error logging

--- arche/config.py
+++ arche/config.py
@@ -29,9 +29,6 @@ levelSystemConsole =	 DEBUG
 levelLogFile =		 DEBUG
 
 formatLogging = "%(levelname)8s | %(lineno)4d | %(name)-15s |: %(message)s"
-
-logFile = "error.log"
-
 """
 
 configDefaultEngine = """
--- arche/debug.py
+++ arche/debug.py
@@ -5,13 +5,14 @@ from logging import *
 import sys
 from os import path
 
+import time
+
 from . import config
 
-formatLogging = "%(lineno)4d | %(asctime)s | %(levelname)8s | %(name)s |: %(message)s"
-levelGameConsole = INFO
-levelSystemConsole = INFO
-levelLogFile = INFO
-logFile = "error.log"
+##formatLogging = "%(lineno)4d | %(asctime)s | %(levelname)8s | %(name)s |: %(message)s"
+##levelGameConsole = INFO
+##levelSystemConsole = INFO
+##levelLogFile = DEBUG
 
 exec(config.loadConfiguration("debug.cfg").read())
 
@@ -20,16 +21,23 @@ alog.setLevel(DEBUG)
 
 console = logging.StreamHandler()
 console.setLevel(levelSystemConsole)
-logfile = logging.FileHandler(logFile)
-logfile.setLevel(levelLogFile)
+##logfile = logging.FileHandler(logFile)
+##logfile.setLevel(levelLogFile)
+
+logfileTimestamp = logging.FileHandler("log/{}.log".format(time.strftime("%y %j %H %M %S %A %b %d")))
+logfileTimestamp.setLevel(levelLogFile)
 
 formatter = logging.Formatter(formatLogging)
 
 console.setFormatter(formatter)
-logfile.setFormatter(formatter)
+##logfile.setFormatter(formatter)
+
+logfileTimestamp.setFormatter(formatter)
 
 alog.addHandler(console)
-alog.addHandler(logfile)
+##alog.addHandler(logfile)
+
+alog.addHandler(logfileTimestamp)
 
 def log(name):
     name = name.replace(".", " ")
@@ -41,7 +49,15 @@ def log(name):
 
 def test(main):
     alog.info("starting tests")
+    criticalError = False
     try:
         main()
+    except SystemExit as e:
+        if e.code:
+            criticalError = True
+            alog.critical(traceback.format_exc())
+        else:
+            alog.info("quit with error code 0")
     except:
+        criticalError = True
         alog.critical(traceback.format_exc())
