6b4ebe4859cc6fc01ab05973dfaa359756a3e1e8
Matthew Robinson <omegachysis@outlook.com>
2014-05-15 16:47:26 -0400

    Simple key reading update

--- arche/control.py
+++ arche/control.py
@@ -9,6 +9,8 @@ def isEventKeydown(event):
     return (event.type == pygame.locals.KEYDOWN)
 
 def keyPressed(key):
+    if isinstance(key, str):
+        key = eval("K_" + key.upper())
     return (pygame.key.get_pressed()[key])
 key = keyPressed
 
